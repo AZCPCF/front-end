af54014b131dfa597020d0378f0df7ac8b80243f
AZCPCF <99.cpcf.99@gmail.com>
2023-10-03 18:11:01 +0330

    update ex_4

--- exercise/ex_4/delDuplicate.js
+++ exercise/ex_4/delDuplicate.js
@@ -1,3 +1,2 @@
 const delDuplicate = (array) => array.sort((a, b) => a - b).filter((item, index) => array.indexOf(item) === index)
-const result = (array) =>  {console.log(delDuplicate(array))}
-export default result
+export default result = (array) =>  {console.log(delDuplicate(array))}
