59a61bf60b2ca1e4692d4b8e36b103fb24858996
AZCPCF <99.cpcf.99@gmail.com>
2023-10-04 20:01:08 +0330

    add response.js

--- exercise/ex_4/functions.js
+++ exercise/ex_4/functions.js
@@ -1,3 +1,8 @@
 const delDuplicate = (array) => array.sort((a, b) => a - b).filter((item, index) => array.indexOf(item) === index)
 const result = (array) => console.log(array)
-export default { delDuplicate, result }
+
+const modules = {
+    delDuplicate,
+    result
+}
+export default modules
--- exercise/ex_4/main.js
+++ exercise/ex_4/main.js
@@ -1,4 +1,5 @@
-import functions from "./functions.js"
+import  modules from "./functions.js"
+const {delDuplicate,result} = modules
 let array1 = [1, 2, 3, 3, 4, 6, 6, 7, 8]
 let array2 = [0, 0, 1, 5, 5, 5, 6, 8, 10]
-functions.result(functions.delDuplicate([...array1,...array2]))
+result(delDuplicate([...array1, ...array2]))
