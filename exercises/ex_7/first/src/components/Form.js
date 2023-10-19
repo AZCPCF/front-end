624130fe29edd1d6e23b7e807377d8bb6f0ad928
AZCPCF <99.cpcf.99@gmail.com>
2023-10-19 11:06:36 +0330

    update ex_7/first

--- exercises/ex_7/first/src/components/Form.js
+++ exercises/ex_7/first/src/components/Form.js
@@ -1,11 +1,12 @@
 import React, { useRef } from 'react'
 export default function Form() {
-    const pRef = useRef(null)
+    const inputRef = useRef(null)
     return (
         <>
-            <span>input : </span>
-            <input type="text" ref={pRef} onChange={e => pRef.current.textContent = e.target.value} />
-            <p>value : <span ref={pRef}></span></p>
+            <div className="App">
+                <input type="text" onChange={e => inputRef.current.innerHTML = 'value :  '+ e.target.value}/>
+                <p ref={inputRef}>value : </p>
+            </div>
         </>
     )
 }
